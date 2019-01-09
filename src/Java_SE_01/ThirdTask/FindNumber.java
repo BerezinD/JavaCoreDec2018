3701879d2af4c15ccc562fe58b8f13f62ba0f524
BerezinD <baron_6600@hotmail.com>
2019-01-09 18:34:24 +0300

    Added user input for third task

--- src/Java_SE_01/ThirdTask/FindNumber.java
+++ src/Java_SE_01/ThirdTask/FindNumber.java
@@ -1,12 +1,55 @@
 package Java_SE_01.ThirdTask;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ * This class is writting to solve only one task - homework from EPAM
+ * @author someOneFromEPAM
+ * @version 2.0
+ * from <i>11.12.2013</i>
+ */
 public class FindNumber {
 
+    /**
+     * This main method for run program
+     * And - with user input
+     * @param args - nothing to use
+     */
     public static void main(String[] args) {
-        FindNumber.printTable(FindNumber.function(0.0,0.0,0.0));
+        double start, end, step;
+        start = 0.0; end = 0.0; step = 0.0;
+
+        System.out.println("Welcome to calculator F(x)!\n" +
+                "To calculate simple function F(x) = tg(2x)-3 inside specific interval\n" +
+                "type next arguments in a correct order:\n" +
+                "Start of interval (enter), End of interval (enter), Step of calculations.");
+
+        Scanner userInput = new Scanner(System.in);
+        while (true){
+            try {
+                System.out.print("start = ");
+                start = userInput.nextDouble();
+                System.out.print("end = ");
+                end = userInput.nextDouble();
+                System.out.print("step = ");
+                step = userInput.nextDouble();
+                break;
+            } catch (InputMismatchException ex){
+                System.out.println("Please use numbers, dude!");
+                userInput.nextLine();
+            }
+
+        }
+        FindNumber.printTable(FindNumber.function(start,end,step));
     }
 
-    //only before 1 million correct
+    /**
+     * Just for print table
+     * @param array - for print one table with two columns
+     *              and in one representation - double numbers
+     *              with 4 digits after point. Works correct in +-10 000 000 not inclusive.
+     */
     public static void printTable(double[][] array){
         System.out.println("\u250C-------------\u252C-------------\u2510");
         System.out.println("\u2502      x      \u2502     F(x)    \u2502");
@@ -23,6 +66,13 @@ public class FindNumber {
         System.out.print  ("\u2514-------------\u2534-------------\u2518");
     }
 
+    /**
+     * Calculate function tg(2x)-3
+     * @param start of interval, inclusive
+     * @param end of interval, inclusive
+     * @param step of calculating function
+     * @return table (array of doubles) ready for print
+     */
     public static double[][] function(double start, double end, double step){
         if (end < start) return  function(end,start,step);
         if (step == 0.0) return new double[][] {{start,Math.tan(2.0*start)-3.0}};
