07567f37c703c9ac578470c9c1790d16ceda0b33
BerezinD <ghost_6600@mail.ru>
2019-02-06 22:08:05 +0300

    Delete Second sheet and add not working methods

--- src/Java_SE_02/FirstToFourthTask/Stationery.java
+++
@@ -1,7 +0,0 @@
-package Java_SE_02.FirstToFourthTask;
-
-public interface Stationery {
-    int getCost();
-    int getSize();
-    String getName();
-}
--- src/Java_SE_03/CrazyLogger.java
+++ src/Java_SE_03/CrazyLogger.java
@@ -1,4 +1,10 @@
 package Java_SE_03;
 
 public class CrazyLogger {
+
+    private StringBuilder log;
+
+    public void findLog(){
+        
+    }
 }
